1117777e206f00b79e775cccc9be7ece23678315
ngkaizhe123 <kaizhe8229@gmail.com>
2026-07-16 15:01:26 +0800

    Implement XGBoost model

--- models/xgboost_model.py
+++ models/xgboost_model.py
@@ -34,7 +34,7 @@ def train_xgboost(X_train, y_train):
     random_search.fit(X_train, y_train)
 
     return (
-    random_search.best_estimator_,
-    random_search.best_params_,
-    random_search.best_score_,
-)
+        random_search.best_estimator_,
+        random_search.best_params_,
+        random_search.best_score_,
+    )
